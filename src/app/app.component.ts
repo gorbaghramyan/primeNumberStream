f73ec61cec61f140cf2b48c6a57de747330ee805
masfuerte818 <67984099+masfuerte818@users.noreply.github.com>
2021-08-23 23:05:25 +0400

    added ui and start stop buttons logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 import { StreamItem as Stream } from './models/StreamItem';
 
 @Component({
@@ -7,36 +7,40 @@ import { StreamItem as Stream } from './models/StreamItem';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   arrPrime: Stream[] = [];
+  primeNumbers$: Subscription = new Subscription();
 
-  ngOnInit(): void {
-    const numbers$ = interval(70);
+  startStream(): void {
+    if (this.arrPrime.length !== 0) {
+      this.arrPrime = []
+    }
+    this.primeNumbers$ = interval(1)
+      .pipe(this.isPrime(this.arrPrime))
+      .subscribe(number => {
+        const item = new Stream(number);
+        this.arrPrime.push(item);
+      });
+  };
 
-    const primeNumbers$ = numbers$.pipe(
-      this.isPrime(this.arrPrime)
-    );
-
-    primeNumbers$.subscribe(x => {
-      const item = new Stream(x);
-      this.arrPrime.push(item);
-    });
+  stopStream(): void {
+    this.primeNumbers$.unsubscribe()
   }
 
   isPrime(arrPrime: Stream[]) {
-    return function <T>(source: Observable<T>): Observable<T> {
-      return new Observable(subscriber => {
+    return function <StreamItem>(source: Observable<StreamItem>): Observable<StreamItem> {
+      return new Observable(observer => {
         source.subscribe({
           next(value) {
             if (isPrime(Number(value), arrPrime)) {
-              subscriber.next(value);
+              observer.next(value);
             }
           },
           error(error) {
-            subscriber.error(error);
+            observer.error(error);
           },
           complete() {
-            subscriber.complete();
+            observer.complete();
           }
         });
       });
@@ -49,11 +53,11 @@ export class AppComponent implements OnInit {
         }
 
         let isPrime = true;
-        arr.map((el, i) => {
+        arr.map(el => {
           if (value % el.value === 0) {
             isPrime = false;
           }
-        })
+        });
 
         return isPrime;
       }
